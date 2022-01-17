764b3d3194f49ec4bf3130842f365cca3d0e33f9
AmesynthEcho <AmesynthEcho@gmail.com>
2022-01-16 23:39:40 -0500

    jan 16 content update + audio index loading update

--- assets/js/indexloading.js
+++ assets/js/indexloading.js
@@ -28,9 +28,25 @@ function appendData(data) {
         // var embedstring = "<img src='https://img.youtube.com/vi/";
         // embedstring += data[i].link;
         // embedstring += "/hqdefault.jpg'/>";
-        var embedstring = "<iframe src='"
+        // var embedstring = "<iframe src='"
+        // embedstring += data[i].link;
+        // embedstring += "' frameborder='0' allow='accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture' allowfullscreen></iframe>";
+        
+        var embedstring = "<iframe\
+            title='Youtube'\
+            aria-hidden='true'\
+            className='embed-vid'\
+            frameBorder='0'\
+            allow='accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture'\
+            allowFullScreen\
+            webkitallowfullscreen='true'\
+            mozallowfullscreen='true'\
+            srcDoc='<style>*{padding:0;margin:0;overflow:hidden}html,body{height:100%}img,span{position:absolute;width:100%;top:0;bottom:0;margin:auto}span{height:1.5em;text-align:center;font:48px/1.5 sans-serif;color:white;text-shadow:0 0 0.5em black}</style><a href=https://www.youtube.com/embed/";
         embedstring += data[i].link;
-        embedstring += "' frameborder='0' allow='accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture' allowfullscreen></iframe>";
+        embedstring += "/?autoplay=1><img src=https://img.youtube.com/vi/"
+        embedstring += data[i].link;
+        embedstring += "/hqdefault.jpg></a>'></iframe>";
+        
         embedvid.innerHTML = embedstring;
 
         // create the info container div
